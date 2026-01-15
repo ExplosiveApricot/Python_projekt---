79c20bc24301e6032a1fb4466f24f186c1d5be85
Explosive <tobias.hoglund@skola.taby.se>
2026-01-15 13:03:50 +0100

    ett till fel fixat

--- spelaren.py
+++ spelaren.py
@@ -9,8 +9,8 @@ class spelaren():
         self.lvl = lvl
         self.exp = exp
         self.stk = stk
-        self.bryggdnedräkning = 0
-        self.boknedräkning = 0
+        self.bryggdnedräkning = bryggdnedräkning
+        self.boknedräkning = boknedräkning
 
 
     @property
@@ -37,17 +37,17 @@ while not klas:
     klassval = input("Ditt val---> ")
 
     if klassval == "1":
-        klass = spelaren(20,100,100,0,0,0,0)
+        klass = spelaren(20,100,100,0,0,0,0,0,0)
         slowprint(f"Random Dansk: Du går bygg och har därmed statsen: {klass.atk} atk, {klass.mhp} hp och {klass.int} int. Din skada ökar också med förlorat hp!")
         klas = True
 
     elif klassval == "2":
-        klass = spelaren(0,100,100,20,5,0,0)
+        klass = spelaren(0,100,100,20,5,0,0,0,0)
         slowprint(f"Random Dansk: Det var alltså du som lukta, du har statsen: {klass.atk} atk, {klass.mhp} hp och {klass.int} int, du  har också {klass.stk} stinknivå.")
         klas = True
 
     elif klassval == "3":
-        klass = spelaren(10,100,100,10,0,0,0)
+        klass = spelaren(10,100,100,10,0,0,0,0,0)
         slowprint(f"Random Dansk: Du går nu sam med statsen: {klass.atk} atk, {klass.mhp} hp och {klass.int} int. Se till att samla aura!")
         klas = True
 
